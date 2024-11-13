bd11c878e99ac89d3b86ef747b1bf4a91820a41a
Syed Farhan Ali <tofarhanali@gmail.com>
2024-11-13 12:03:52 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var name = "Farhan"; // var is a function scope and it can be redeclare and updated
+let email = "tofarhanali@gmail.com"; //let is a block scope and it cannot be redeclare but updated
+const id = 1; //const is a block scope and it cannot be redecare and not updated
+
+let account; //its output is undefined
+
+console.table([name, email, id, account]);
+
